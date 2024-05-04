50322bd72cb3077e917aea6036ff2134a8d8b088
pretzelogical <yingyangyo266@gmail.com>
2024-05-03 19:34:01 -0500

    task 2

---
+++ react_intro/task_2/dashboard/src/Notifications.js
@@ -0,0 +1,10 @@
+import './Notifications.css'
+
+
+export default function Notifications () {
+  return (
+    <div className="notifications">
+      <p>Here is the list of notifications</p>
+    </div>
+  )
+}
