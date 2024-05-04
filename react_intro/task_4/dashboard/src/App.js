b3bb3a3d8d86fd3be6076dec6c58ddadb3ef1ec4
pretzelogical <yingyangyo266@gmail.com>
2024-05-04 02:54:47 -0500

    task 3, start task 4

---
+++ react_intro/task_4/dashboard/src/App.js
@@ -0,0 +1,50 @@
+import './App.css';
+import Notifications from './Notifications';
+import HolbertonLogo from './holberton_logo.jpg';
+import { getFooterCopy } from './utils';
+
+
+function App() {
+  return (
+    <>
+      <div className="root-notifications">
+        <Notifications />
+      </div>
+      <div className='App-header'>
+        <img
+        src={HolbertonLogo}
+        alt='Holberton Logo'
+        width={256}
+        height={256}></img>
+        <h1>School Dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <div className="App-body-login">
+          <div>
+            <label
+            htmlFor="email"
+            onClick={() => document.getElementById('email').focus()}
+            >Email:</label>
+            <input type="email" name="email" id="email" />
+          </div>
+          <div>
+            <label
+            htmlFor="password"
+            onClick={() => document.getElementById('password').focus()}
+            >Password:</label>
+            <input type="password" name="password" id="password" />
+          </div>
+          <div>
+            <button id='confirm'>OK</button>
+          </div>
+        </div>
+      </div>
+      <div className="App-footer">
+        <p>Copyright 2020 - {getFooterCopy(true)}</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
