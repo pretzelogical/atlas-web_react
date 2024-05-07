69cd5aba6a12d619c55e29546f6b2dfd8ddb12b4
pretzelogical <yingyangyo266@gmail.com>
2024-05-06 19:37:45 -0500

    now able to get to jsx parsing

--- react_intro/task_5/dashboard/config/setupTests.mjs
+++ react_intro/task_5/dashboard/config/setupTests.mjs
@@ -1,5 +1,5 @@
 import Enzyme from 'enzyme';
-import Adapter from '@cfaester/enzyme-adapter-react-18';
+import Adapter from 'enzyme-adapter-react-16';
 
 // NOTE: enzyme isn't working here, probably because it's ded
 Enzyme.configure({ adapter: new Adapter() });
