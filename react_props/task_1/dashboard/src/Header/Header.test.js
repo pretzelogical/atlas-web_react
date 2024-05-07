b9cd6105b18f84c292c595bda2fdfe60f3d51cd2
pretzelogical <yingyangyo266@gmail.com>
2024-05-06 22:37:40 -0500

    task 1 add tests

--- react_props/task_1/dashboard/src/Header/Header.test.js
+++ react_props/task_1/dashboard/src/Header/Header.test.js
@@ -1,1 +1,15 @@
-test('blank test', () => void(0));
+import React from 'react';
+import { shallow } from "../../config/setupTests.mjs";
+import Header from './Header.js';
+
+
+test('Header renders', () => {
+  const wrapper = shallow(<Header />);
+  expect(wrapper.exists()).toBe(true);
+});
+
+test('Header renders img and h1 tags', () => {
+  const wrapper = shallow(<Header />);
+  expect(wrapper.children().find('img').exists()).toBe(true);
+  expect(wrapper.children().find('h1').exists()).toBe(true);
+});
