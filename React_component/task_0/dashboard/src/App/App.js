f850592afad0a9537e1eb4b57f17fac23cb019f0
pretzelogical <yingyangyo266@gmail.com>
2024-05-09 10:45:38 -0500

    React_component task_0

--- React_component/task_0/dashboard/src/App/App.js
+++ React_component/task_0/dashboard/src/App/App.js
@@ -7,59 +7,66 @@ import Login from '../Login/Login.js';
 import PropTypes from 'prop-types';
 import CourseList from '../CourseList/CourseList.js';
 
-function App({ isLoggedin = false }) {
-  const listCourses = [
-    {
-      id: 1,
-      name: 'ES6',
-      credit: 60,
-    },
-    {
-      id: 2,
-      name: 'Webpack',
-      credit: 20,
-    },
-    {
-      id: 3,
-      name: 'React',
-      credit: 40,
-    },
-  ];
+const listCourses = [
+  {
+    id: 1,
+    name: 'ES6',
+    credit: 60,
+  },
+  {
+    id: 2,
+    name: 'Webpack',
+    credit: 20,
+  },
+  {
+    id: 3,
+    name: 'React',
+    credit: 40,
+  },
+];
 
-  const listNotifications = [
-    {
-      id: 1,
-      type: 'default',
-      value: 'Default Notification'
-    },
-    {
-      id: 2,
-      type: 'urgent',
-      value: 'Urgent Notification'
-    },
-    {
-      id: 3,
-      type: 'urgent',
-      html: { __html: '<b>Html</b> notification' }
-    }
-  ];
-  return (
-    <>
-      <div className="root-notifications"></div>
-      <div className="App-header">
-        <Header />
-        <Notifications listNotifications={listNotifications}/>
-      </div>
-      <div className="App-body">
-        <main>
-          {isLoggedin ? <CourseList listCourses={listCourses} /> : <Login />}
-        </main>
-      </div>
-      <div className="App-footer">
-        <Footer />
-      </div>
-    </>
-  );
+const listNotifications = [
+  {
+    id: 1,
+    type: 'default',
+    value: 'Default Notification',
+  },
+  {
+    id: 2,
+    type: 'urgent',
+    value: 'Urgent Notification',
+  },
+  {
+    id: 3,
+    type: 'urgent',
+    html: { __html: '<b>Html</b> notification' },
+  },
+];
+
+class App extends React.Component {
+  static defaultProps = {
+    isLoggedIn: false
+  }
+
+  render() {
+    return (
+      <>
+        <div className="root-notifications"></div>
+        <div className="App-header">
+          <Header />
+          <Notifications listNotifications={listNotifications} />
+        </div>
+        <div className="App-body">
+          <main>
+            {this.props.isLoggedin ? <CourseList listCourses={listCourses} /> : <Login />}
+          </main>
+        </div>
+        <div className="App-footer">
+          <Footer />
+        </div>
+      </>
+    );
+  }
 }
 
 App.propTypes = {
