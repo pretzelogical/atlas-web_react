7b2bb40ae4c149df4acb81e0b67ef9aaccac4461
pretzelogical <yingyangyo266@gmail.com>
2024-05-10 19:54:35 -0500

    Add NotificationItem tests

--- React_component/task_2/dashboard/src/Notifications/NotificationItem.test.js
+++ React_component/task_2/dashboard/src/Notifications/NotificationItem.test.js
@@ -1,6 +1,8 @@
 import React from "react";
 import NotificationItem from "./NotificationItem.js";
 import { shallow } from "../../config/setupTests.mjs";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { jest } from "../../config/setupTests.mjs";
 
 
 test('NotificationItem renders', () => {
@@ -20,4 +22,30 @@ test('NotificationItem renders the right html', () => {
   expect(wrapper.html()).toBe(
     '<li data-priority="basic" class="notification-basic"><p>Test</p></li>'
   );
+});
+
+
+test('NotificationItem calls markAsRead when clicked on', () => {
+  const spyMarkAsRead = jest.fn();
+  render(<NotificationItem value={'test'} type={'basic'} markAsRead={spyMarkAsRead} />);
+  const li_elems = screen.getAllByRole('listitem');
+
+  li_elems.forEach((li) => {
+    fireEvent.click(li);
+  });
+
+  expect(spyMarkAsRead).toHaveBeenCalled();
+});
+
+
+test('NotificationItem calls markAsRead with correct id argument when clicked on', () => {
+  const mockConsoleLog = jest.spyOn(console, 'log').mockImplementation(() => void(0));
+  render(<NotificationItem value={'test'} type={'basic'} markAsRead={() => console.log('ID')} />);
+  const li_elems = screen.getAllByRole('listitem');
+
+  li_elems.forEach((li) => {
+    fireEvent.click(li);
+  });
+
+  expect(mockConsoleLog).toHaveBeenCalledWith('ID');
 });
