ee1a0fabf7d7b0a82901de438d569e66ddcf6b10
pretzelogical <yingyangyo266@gmail.com>
2024-05-10 20:14:57 -0500

    task 3 setup

---
+++ React_component/task_3/dashboard/src/CourseList/CourseListRow.test.js
@@ -0,0 +1,39 @@
+import React from "react";
+import CourseListRow from "./CourseListRow.js";
+import { shallow } from "../../config/setupTests.mjs";
+
+
+test(
+  'CourseListRow correctly renders one cell when isHeader = true',
+  () => {
+    const wrapper = shallow(<CourseListRow isHeader={true} />);
+    expect(wrapper.is('tr')).toBe(true);
+    expect(wrapper.find('th').length).toBe(1);
+});
+
+
+test(
+  'CourseListRow correctly renders two cells when isHeader = true & textSecondCell is present',
+  () => {
+    const wrapper = shallow(
+      <CourseListRow
+        isHeader={true}
+        textSecondCell={'test'}
+      />
+    );
+    expect(wrapper.is('tr')).toBe(true);
+    expect(wrapper.find('th').length).toBe(2);
+});
+
+
+test(
+  'CourseListRow correctly renders two td elements within a tr element',
+  () => {
+    const wrapper = shallow(
+      <CourseListRow
+        isHeader={false}
+      />
+    );
+    expect(wrapper.is('tr')).toBe(true);
+    expect(wrapper.find('td').length).toBe(2);
+});
---
+++ React_component/task_3/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,85 @@
+import './Notifications.css';
+import NotificationItem from './NotificationItem.js';
+import React from 'react';
+import PropTypes from 'prop-types';
+import NotificationItemShape from './NotificationItemShape.js';
+
+
+class Notifications extends React.Component {
+  static defaultProps = {
+    displayDrawer: false,
+    listNotifications: []
+  }
+
+  constructor(props) {
+    super(props);
+    this.markAsRead = this.markAsRead.bind(this);
+  }
+
+  markAsRead(id) {
+    console.log(`Notification ${id} has been marked as read`);
+  }
+
+  render() {
+    return (
+      <div className="notifications-display">
+        <div className="menuItem">
+          <p>Your notifications</p>
+        </div>
+        {this.props.displayDrawer ? (
+          <div className="notifications">
+            <button
+              style={{
+                background: 'none',
+                border: 'none',
+                fontSize: '20px',
+                cursor: 'pointer',
+                marginLeft: 'auto',
+                marginBottom: '0',
+                color: 'gray',
+              }}
+              aria-label="close"
+              onClick={() => console.log('Close button has been clicked')}
+            >
+              x
+            </button>
+            <p>Here is the list of notifications</p>
+            <ul>
+              {this.props.listNotifications.length === 0 ? (
+                <li>No new notification for now</li>
+              ) : (
+                this.props.listNotifications.map((notif) => {
+                  if (!notif.value) {
+                    return (
+                      <NotificationItem
+                        type={notif.type}
+                        html={notif.html}
+                        key={notif.id}
+                        markAsRead={() => this.markAsRead(notif.id)}
+                      />
+                    );
+                  }
+                  return (
+                    <NotificationItem
+                      type={notif.type}
+                      value={notif.value}
+                      key={notif.id}
+                      markAsRead={() => this.markAsRead(notif.id)}
+                    />
+                  );
+                })
+              )}
+            </ul>
+          </div>
+        ) : null}
+      </div>
+    );
+  }
+}
+
+Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
+  listNotifications: PropTypes.arrayOf(PropTypes.shape(NotificationItemShape)),
+};
+
+export default Notifications;
