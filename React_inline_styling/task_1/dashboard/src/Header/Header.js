1d3c4766761c0c111fb4d248bd08c154b7fbd06b
pretzelogical <yingyangyo266@gmail.com>
2024-05-14 13:35:33 -0500

    add Header inline styling

--- React_inline_styling/task_1/dashboard/src/Header/Header.js
+++ React_inline_styling/task_1/dashboard/src/Header/Header.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import HolbertonLogo from '../assets/holberton_logo.jpg';
-import './Header.css';
+import { StyleSheet, css } from 'aphrodite';
+
+const headerStyles = StyleSheet.create({
+  header: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  h1: {
+    color: '#dd283f'
+  }
+})
 
 export default function Header() {
   return (
-    <header>
+    <header className={css(headerStyles.header)}>
       <img
         src={HolbertonLogo}
         alt="Holberton Logo"
         width={256}
         height={256}
       ></img>
-      <h1>School Dashboard</h1>
+      <h1 className={css(headerStyles.h1)}>School Dashboard</h1>
     </header>
   );
 }
--- React_inline_styling/task_1/dashboard/src/Header/Header.test.js
+++ React_inline_styling/task_1/dashboard/src/Header/Header.test.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { shallow } from "../../config/setupTests.mjs";
 import Header from './Header.js';
+import { StyleSheetTestUtils } from "aphrodite";
 
+beforeEach(() => {
+  StyleSheetTestUtils.suppressStyleInjection();
+});
+
+afterEach(() => {
+  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+});
 
 test('Header renders', () => {
   const wrapper = shallow(<Header />);
