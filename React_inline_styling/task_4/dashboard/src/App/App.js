cf9a0becb94866c3e021efd917b04b1eddbe4c41
pretzelogical <yingyangyo266@gmail.com>
2024-05-14 17:33:06 -0500

    start task 4

---
+++ React_inline_styling/task_4/dashboard/src/App/App.js
@@ -0,0 +1,111 @@
+import Notifications from '../Notifications/Notifications.js';
+import React from 'react';
+import Footer from '../Footer/Footer.js';
+import Header from '../Header/Header.js';
+import Login from '../Login/Login.js';
+import PropTypes from 'prop-types';
+import CourseList from '../CourseList/CourseList.js';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom.js';
+import BodySection from '../BodySection/BodySection.js';
+import { StyleSheet, css } from 'aphrodite';
+
+const listCourses = [
+  {
+    id: 1,
+    name: 'ES6',
+    credit: 60,
+  },
+  {
+    id: 2,
+    name: 'Webpack',
+    credit: 20,
+  },
+  {
+    id: 3,
+    name: 'React',
+    credit: 40,
+  },
+];
+
+const listNotifications = [
+  {
+    id: 1,
+    type: 'default',
+    value: 'Default Notification',
+  },
+  {
+    id: 2,
+    type: 'urgent',
+    value: 'Urgent Notification',
+  },
+  {
+    id: 3,
+    type: 'urgent',
+    html: { __html: '<b>Html</b> notification' },
+  },
+];
+
+const appStyle = StyleSheet.create({
+  appHeader: {
+    borderBottom: '3px solid #dd283f',
+    display: 'flex',
+    alignItems: 'center',
+    paddingBottom: '16px',
+  },
+  appBody: {
+    margin: '4rem',
+    marginBottom: 'auto'
+  },
+});
+
+class App extends React.Component {
+  static defaultProps = {
+    isLoggedIn: false,
+    logOut: () => void(0)
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', (e) => {
+      if (e.ctrlKey && e.key === 'h') {
+        alert('Logging you out');
+        this.props.logOut();
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', document);
+  }
+
+  render() {
+    return (
+      <>
+        <div className={css(appStyle.appHeader)}>
+          <Header />
+          <Notifications listNotifications={listNotifications} />
+        </div>
+        <div className={css(appStyle.appBody)}>
+          <main>
+            <BodySectionWithMarginBottom>
+              {this.props.isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+            </BodySectionWithMarginBottom>
+            <BodySection>
+              <h2>News from the School</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo provident possimus numquam autem.</p>
+            </BodySection>
+          </main>
+        </div>
+        <div className="App-footer">
+          <Footer />
+        </div>
+      </>
+    );
+  }
+}
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func
+};
+
+export default App;
---
+++ React_inline_styling/task_4/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,150 @@
+import NotificationItem from './NotificationItem.js';
+import React from 'react';
+import PropTypes from 'prop-types';
+import NotificationItemShape from './NotificationItemShape.js';
+import { StyleSheet, css } from 'aphrodite';
+
+const notificationsStyles = StyleSheet.create({
+  notificationsDisplay: {
+    // display: 'flex',
+    top: '4px',
+    position: 'absolute',
+    flexFlow: 'column',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    width: '100%',
+    zIndex: '1000',
+    '@media (min-width: 900px)': {
+      marginRight: '2rem',
+      marginBottom: 'auto',
+      marginLeft: 'auto',
+      right: '20px',
+      top: '0'
+    }
+  },
+
+  notifications: {
+    display: 'flex',
+    position: 'absolute',
+    flexFlow: 'column',
+    paddingBottom: '0.3rem',
+    width: '100vw',
+    height: 'calc(95vh + 32px)',
+    top: '0',
+    backgroundColor: 'white',
+    '@media (min-width: 900px)': {
+      border: '2px dashed black',
+      width: 'min(30vw, 400px)',
+      height: 'auto',
+      padding: '1rem',
+      right: '-20px',
+      top: '50px'
+    }
+  },
+
+  notificationsUl: {
+    marginTop: 0,
+    paddingLeft: '0px',
+    '@media (min-width: 900px)': {
+      marginLeft: '20px'
+    }
+  },
+
+  menuItem: {
+    position: 'absolute',
+    top: '-4px',
+    right: '30px',
+    whiteSpace: 'nowrap',
+    zIndex: '-1',
+    '@media (min-width: 900px)': {
+      top: '0px',
+      right: '-20px'
+    }
+  }
+});
+
+
+class Notifications extends React.Component {
+  static defaultProps = {
+    displayDrawer: false,
+    listNotifications: []
+  }
+
+  constructor(props) {
+    super(props);
+    this.markAsRead = this.markAsRead.bind(this);
+  }
+
+  markAsRead(id) {
+    console.log(`Notification ${id} has been marked as read`);
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return (
+      nextProps.listNotifications.length > this.props.listNotifications.length
+    );
+  }
+
+  render() {
+    return (
+      <div className={css(notificationsStyles.notificationsDisplay)}>
+        <div className={css(notificationsStyles.menuItem)}>
+          <p>Your notifications</p>
+        </div>
+        {this.props.displayDrawer ? (
+          <div className={css(notificationsStyles.notifications)}>
+            <button
+              style={{
+                background: 'none',
+                border: 'none',
+                fontSize: '20px',
+                cursor: 'pointer',
+                marginLeft: 'auto',
+                marginBottom: '0',
+                color: 'gray',
+              }}
+              aria-label="close"
+              onClick={() => console.log('Close button has been clicked')}
+            >
+              x
+            </button>
+            <p>Here is the list of notifications</p>
+            <ul className={css(notificationsStyles.notificationsUl)}>
+              {this.props.listNotifications.length === 0 ? (
+                <li>No new notification for now</li>
+              ) : (
+                this.props.listNotifications.map((notif) => {
+                  if (!notif.value) {
+                    return (
+                      <NotificationItem
+                        type={notif.type}
+                        html={notif.html}
+                        key={notif.id}
+                        markAsRead={() => this.markAsRead(notif.id)}
+                      />
+                    );
+                  }
+                  return (
+                    <NotificationItem
+                      type={notif.type}
+                      value={notif.value}
+                      key={notif.id}
+                      markAsRead={() => this.markAsRead(notif.id)}
+                    />
+                  );
+                })
+              )}
+            </ul>
+          </div>
+        ) : null}
+      </div>
+    );
+  }
+}
+
+Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
+  listNotifications: PropTypes.arrayOf(PropTypes.shape(NotificationItemShape)),
+};
+
+export default Notifications;
