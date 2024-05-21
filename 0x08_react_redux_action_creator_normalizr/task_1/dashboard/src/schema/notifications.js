207bc02f758def355683d4221e22f1d79f5fcfef
pretzelogical <yingyangyo266@gmail.com>
2024-05-21 16:15:27 -0500

    normalize notificaions json object with normalizr

--- 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
+++ 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -1,4 +1,16 @@
-import notifications from '../../notifications.json';
+import notificationsJSON from '../../notifications.json';
+import { normalize, schema } from 'normalizr';
+
+const user = new schema.Entity('users');
+const message = new schema.Entity(
+  'messages',
+  {},
+  { idAttribute: 'guid' }
+);
+const notification = new schema.Entity('notifications', {
+  author: user,
+  context: message
+});
 
 
 /**
@@ -9,6 +21,9 @@ import notifications from '../../notifications.json';
  * @returns {array}
  */
 export function getAllNotificationsByUser(userId) {
-  return notifications.filter((notifItem) => notifItem.author.id === userId)
+  return notificationsJSON.filter((notifItem) => notifItem.author.id === userId)
     .map((notifItem) => notifItem.context);
 }
+
+const notifications = normalize(notificationsJSON, [notification]);
+export default notifications;
