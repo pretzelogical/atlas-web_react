5bad8651d095c089389697eac7d4642b6b1745b5
pretzelogical <yingyangyo266@gmail.com>
2024-05-22 11:25:17 -0500

    Add action creators and action types to handle login success and failure

--- 0x08_react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.js
+++ 0x08_react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.js
@@ -5,29 +5,50 @@ import store from '../store.js';
 export const login = (email, password) => {
   return {
     type: ACTION_TYPES.LOGIN,
-    user: { email, password }
-  }
-}
+    user: { email, password },
+  };
+};
 
 export const logout = () => {
   return {
     type: ACTION_TYPES.LOGOUT,
-  }
-}
+  };
+};
 
 export const displayNotificationDrawer = () => {
   return {
-    type: ACTION_TYPES.DISPLAY_NOTIFICATION_DRAWER
-  }
-}
+    type: ACTION_TYPES.DISPLAY_NOTIFICATION_DRAWER,
+  };
+};
 
 export const hideNotificationDrawer = () => {
   return {
-    type: ACTION_TYPES.HIDE_NOTIFICATION_DRAWER
-  }
-}
+    type: ACTION_TYPES.HIDE_NOTIFICATION_DRAWER,
+  };
+};
+
+export const loginSuccess = () => ({
+  type: ACTION_TYPES.LOGIN_SUCCESS,
+});
+
+export const loginFailure = () => ({
+  type: ACTION_TYPES.LOGIN_FAILURE,
+});
+
+export const loginRequest = (email, password) => {
+  store.dispatch(login(email, password));
+  fetch('http://localhost:8080/static/login-success.json')
+    .then(() => store.dispatch(loginSuccess()))
+    .catch(() => store.dispatch(loginFailure()));
+};
 
 export const boundLogin = bindActionCreators(login, store.dispatch);
 export const boundLogout = bindActionCreators(logout, store.dispatch);
-export const boundDisplayNotificationDrawer = bindActionCreators(displayNotificationDrawer, store.dispatch);
-export const boundHideNotificationDrawer = bindActionCreators(hideNotificationDrawer, store.dispatch);
+export const boundDisplayNotificationDrawer = bindActionCreators(
+  displayNotificationDrawer,
+  store.dispatch,
+);
+export const boundHideNotificationDrawer = bindActionCreators(
+  hideNotificationDrawer,
+  store.dispatch,
+);
